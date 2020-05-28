7dc75959d95cbbcf12fcff92657c71d31729e685
SpessotoVitor <spessoto97@gmail.com>
2020-05-28 18:02:07 -0300

    Initial Commit

---
+++ TheLogger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheLogger
+//
+//  Created by Vitor Spessoto on 5/28/20.
+//  Copyright © 2020 Vitor Spessoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TheLoggerTests/TheLoggerTests.swift
@@ -0,0 +1,34 @@
+//
+//  TheLoggerTests.swift
+//  TheLoggerTests
+//
+//  Created by Vitor Spessoto on 5/28/20.
+//  Copyright © 2020 Vitor Spessoto. All rights reserved.
+//
+
+import XCTest
+@testable import TheLogger
+
+class TheLoggerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
